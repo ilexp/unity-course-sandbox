02891a49607dfb3b9edeca3349a54f60c0cf6796
Adam <adam@adamslair.net>
2018-03-24 15:00:39 +0100

    HelloWorld Script WiP

--- Sandbox/Assets/CSharp/HelloWorld.cs
+++ Sandbox/Assets/CSharp/HelloWorld.cs
@@ -6,9 +6,45 @@ namespace AwesomeProject
 {
 	public class HelloWorld : MonoBehaviour
 	{
+		private GameObject spawnedObj = null;
+
 		private void Start()
 		{
 			Debug.LogFormat("Hello World");
+			Debug.LogFormat("Number: {0}", 123);
+			Debug.LogFormat("String: {0}", "Test");
+
+			int numberVariable = 234;
+			string stringVariable = "Something";
+			Debug.LogFormat("Number variable: {0}", numberVariable);
+			Debug.LogFormat("String variable: {0}", stringVariable);
+
+			for (int i = 0; i < 10; i++)
+			{
+				Debug.LogFormat("Counter: {0}", i);
+				if (i > 5)
+				{
+					Debug.LogFormat("{0} is greater than 5", i);
+				}
+			}
+
+			this.spawnedObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			this.spawnedObj.transform.SetParent(this.transform);
+
+			MeshRenderer renderer = this.spawnedObj.GetComponent<MeshRenderer>();
+			renderer.material.color = Color.red;
+		}
+		private void Update()
+		{
+			MeshRenderer renderer = this.spawnedObj.GetComponent<MeshRenderer>();
+			if (this.spawnedObj.transform.position.x < 0.0f)
+			{
+				renderer.material.color = Color.green;
+			}
+			else
+			{
+				renderer.material.color = Color.red;
+			}
 		}
 	}
 }
