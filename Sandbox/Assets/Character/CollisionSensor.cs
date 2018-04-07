1235ca6e606e672d270bed4ee35a0fb8fd864c9b
Adam <adam@adamslair.net>
2018-04-07 19:28:13 +0200

    Materials, Main Update, CharacterController Movement

---
+++ Sandbox/Assets/Character/CollisionSensor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AwesomeProject
+{
+	public class CollisionSensor : MonoBehaviour
+	{
+		private int overlapCounter = 0;
+
+		public bool IsTriggered
+		{
+			get { return this.overlapCounter > 0; }
+		}
+
+		private void UpdateDebugDisplay()
+		{
+			MeshRenderer renderer = this.GetComponent<MeshRenderer>();
+			if (this.IsTriggered)
+				renderer.material.color = Color.red;
+			else
+				renderer.material.color = Color.white;
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			this.overlapCounter++;
+			this.UpdateDebugDisplay();
+		}
+		private void OnTriggerExit(Collider other)
+		{
+			this.overlapCounter--;
+			this.UpdateDebugDisplay();
+		}
+	}
+}
--- Sandbox/Assets/Character/FirstPersonCharacterController.cs
+++ Sandbox/Assets/Character/FirstPersonCharacterController.cs
@@ -7,9 +7,13 @@ namespace AwesomeProject
 	public class FirstPersonCharacterController : MonoBehaviour
 	{
 		[SerializeField] private float jumpForce = 4.0f;
-		[SerializeField] private float moveSpeed = 10.0f;
-		[SerializeField] private float lookSpeed = 6.0f;
+		[SerializeField] private float moveAcceleration = 15.0f;
+		[SerializeField] private float moveSpeed = 6.0f;
+		[SerializeField] private float lookSpeed = 5.0f;
 		[SerializeField] private Transform head = null;
+		[SerializeField] private CollisionSensor floorSensor = null;
+
+		private float lookAngle = 0.0f;
 
 		private void Start()
 		{
@@ -20,6 +24,7 @@ namespace AwesomeProject
 			Rigidbody body = this.GetComponent<Rigidbody>();
 			//body.velocity = this.transform.forward;
 
+			bool isOnGround = this.floorSensor.IsTriggered;
 			Vector2 moveInput = new Vector2(
 				Input.GetAxis("Horizontal"),
 				Input.GetAxis("Vertical"));
@@ -30,22 +35,33 @@ namespace AwesomeProject
 			if (moveInput.magnitude > 1.0f)
 				moveInput /= moveInput.magnitude;
 
-			Vector3 targetVelocity = Vector3.zero;
-			targetVelocity += this.transform.forward * moveInput.y * this.moveSpeed;
-			targetVelocity += this.transform.right * moveInput.x * this.moveSpeed;
-			Vector3 velocity = body.velocity;
-			velocity.x = targetVelocity.x;
-			velocity.z = targetVelocity.z;
-			body.velocity = velocity;
+			Vector3 targetVelocity = 
+				this.transform.forward * moveInput.y * this.moveSpeed +
+				this.transform.right * moveInput.x * this.moveSpeed;
+			//Vector3 velocity = body.velocity;
+			//velocity.x = targetVelocity.x;
+			//velocity.z = targetVelocity.z;
+			//body.velocity = velocity;
+			Vector3 moveForce = Vector3.zero;
+			moveForce.x = (targetVelocity.x - body.velocity.x) * this.moveAcceleration;
+			moveForce.z = (targetVelocity.z - body.velocity.z) * this.moveAcceleration;
+			if (!isOnGround)
+			{
+				moveForce *= 0.1f;
+			}
+			body.AddForce(moveForce, ForceMode.Acceleration);
 
 			Quaternion rotation = body.rotation;
 			rotation *= Quaternion.AngleAxis(lookInput.x * this.lookSpeed, Vector3.up);
 			body.MoveRotation(rotation);
 
-			this.head.rotation *= Quaternion.AngleAxis(-lookInput.y * this.lookSpeed, Vector3.right);
+			this.lookAngle = Mathf.Clamp(this.lookAngle - lookInput.y * this.lookSpeed, -90.0f, 90.0f);
+			this.head.localRotation = Quaternion.Euler(this.lookAngle, 0.0f, 0.0f);
 
-			if (jumpInput)
+			if (isOnGround && jumpInput)
+			{
 				body.AddRelativeForce(Vector3.up * this.jumpForce, ForceMode.VelocityChange);
+			}
 		}
 	}
 }
