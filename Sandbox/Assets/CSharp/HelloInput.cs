5f9c9b01a2b3d7c62c99ec4dc02c912d8ed064c5
Adam <adam@adamslair.net>
2018-04-08 14:00:22 +0200

    HelloInput, Smooth Character Controller

---
+++ Sandbox/Assets/CSharp/HelloInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AwesomeProject
+{
+	public class HelloInput : MonoBehaviour
+	{
+		[SerializeField] private float moveSpeed = 3.0f;
+		[SerializeField] private float rotateSpeed = 90.0f;
+
+		private void Update()
+		{
+			Vector2 moveInput = new Vector2(
+				Input.GetAxis("Horizontal"),
+				Input.GetAxis("Vertical"));
+
+			Vector3 pos = this.transform.position;
+			pos += this.transform.forward * moveInput.y * this.moveSpeed * Time.deltaTime;
+			this.transform.position = pos;
+
+			Quaternion rotation = this.transform.rotation;
+			rotation *= Quaternion.AngleAxis(moveInput.x * this.rotateSpeed * Time.deltaTime, Vector3.up);
+			this.transform.rotation = rotation;
+		}
+	}
+}
--- Sandbox/Assets/Character/FirstPersonCharacterController.cs
+++ Sandbox/Assets/Character/FirstPersonCharacterController.cs
@@ -7,24 +7,24 @@ namespace AwesomeProject
 	public class FirstPersonCharacterController : MonoBehaviour
 	{
 		[SerializeField] private float jumpForce = 4.0f;
-		[SerializeField] private float moveAcceleration = 15.0f;
 		[SerializeField] private float moveSpeed = 6.0f;
-		[SerializeField] private float lookSpeed = 5.0f;
+		[SerializeField] private float moveAcceleration = 10.0f;
+		[SerializeField] private float lookSensitivity = 5.0f;
+		[SerializeField] private float lookAcceleration = 25.0f;
 		[SerializeField] private Transform head = null;
 		[SerializeField] private CollisionSensor floorSensor = null;
 
-		private float lookAngle = 0.0f;
+		private Vector2 targetLookAngle = Vector2.zero;
+		private Vector3 targetMove = Vector3.zero;
+		private bool triggerJump = false;
 
 		private void Start()
 		{
+			this.targetLookAngle.x = this.transform.rotation.eulerAngles.y;
 			Cursor.lockState = CursorLockMode.Locked;
 		}
 		private void Update()
 		{
-			Rigidbody body = this.GetComponent<Rigidbody>();
-			//body.velocity = this.transform.forward;
-
-			bool isOnGround = this.floorSensor.IsTriggered;
 			Vector2 moveInput = new Vector2(
 				Input.GetAxis("Horizontal"),
 				Input.GetAxis("Vertical"));
@@ -35,32 +35,48 @@ namespace AwesomeProject
 			if (moveInput.magnitude > 1.0f)
 				moveInput /= moveInput.magnitude;
 
-			Vector3 targetVelocity = 
-				this.transform.forward * moveInput.y * this.moveSpeed +
-				this.transform.right * moveInput.x * this.moveSpeed;
-			//Vector3 velocity = body.velocity;
-			//velocity.x = targetVelocity.x;
-			//velocity.z = targetVelocity.z;
-			//body.velocity = velocity;
-			Vector3 moveForce = Vector3.zero;
-			moveForce.x = (targetVelocity.x - body.velocity.x) * this.moveAcceleration;
-			moveForce.z = (targetVelocity.z - body.velocity.z) * this.moveAcceleration;
+			this.targetMove =
+				this.transform.forward * moveInput.y +
+				this.transform.right * moveInput.x;
+			this.targetLookAngle.x += lookInput.x * this.lookSensitivity;
+			this.targetLookAngle.y += lookInput.y * -this.lookSensitivity;
+			this.targetLookAngle.y = Mathf.Clamp(this.targetLookAngle.y, -90.0f, 90.0f);
+			if (jumpInput) this.triggerJump = true;
+		}
+		private void FixedUpdate()
+		{
+			Rigidbody body = this.GetComponent<Rigidbody>();
+			bool isOnGround = this.floorSensor.IsTriggered;
+
+			//Vector3 targetBodyVelocity = this.targetMove * this.moveSpeed;
+			//targetBodyVelocity.y = bodyVelocity.y;
+			//body.velocity = targetBodyVelocity;
+			//body.MoveRotation(Quaternion.Euler(0.0f, this.targetLookAngle.x, 0.0f));
+			//this.head.localRotation = Quaternion.Euler(this.targetLookAngle.y, 0.0f, 0.0f);
+
+			Vector3 bodyVelocity = body.velocity;
+			Vector3 targetBodyVelocity = this.targetMove * this.moveSpeed;
+			targetBodyVelocity.y = bodyVelocity.y;
 			if (!isOnGround)
+				body.velocity = Vector3.Lerp(bodyVelocity, targetBodyVelocity, this.moveAcceleration * Time.fixedDeltaTime * 0.1f);
+			else
+				body.velocity = Vector3.Lerp(bodyVelocity, targetBodyVelocity, this.moveAcceleration * Time.fixedDeltaTime);
+			
+			Quaternion bodyRotation = body.rotation;
+			Quaternion targetBodyRotation = Quaternion.Euler(0.0f, this.targetLookAngle.x, 0.0f);
+			body.MoveRotation(Quaternion.Lerp(bodyRotation, targetBodyRotation, this.lookAcceleration * Time.fixedDeltaTime));
+			
+			Quaternion headRotation = this.head.localRotation;
+			Quaternion targetHeadRotation = Quaternion.Euler(this.targetLookAngle.y, 0.0f, 0.0f);
+			this.head.localRotation = Quaternion.Lerp(headRotation, targetHeadRotation, this.lookAcceleration * Time.fixedDeltaTime);
+
+			if (this.triggerJump)
 			{
-				moveForce *= 0.1f;
-			}
-			body.AddForce(moveForce, ForceMode.Acceleration);
-
-			Quaternion rotation = body.rotation;
-			rotation *= Quaternion.AngleAxis(lookInput.x * this.lookSpeed, Vector3.up);
-			body.MoveRotation(rotation);
-
-			this.lookAngle = Mathf.Clamp(this.lookAngle - lookInput.y * this.lookSpeed, -90.0f, 90.0f);
-			this.head.localRotation = Quaternion.Euler(this.lookAngle, 0.0f, 0.0f);
-
-			if (isOnGround && jumpInput)
-			{
-				body.AddRelativeForce(Vector3.up * this.jumpForce, ForceMode.VelocityChange);
+				this.triggerJump = false;
+				if (isOnGround)
+				{
+					body.AddForce(Vector3.up * this.jumpForce, ForceMode.VelocityChange);
+				}
 			}
 		}
 	}
